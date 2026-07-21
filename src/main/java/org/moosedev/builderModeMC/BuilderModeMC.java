1a3d887af59b31fdd345841205ab8c040784fbeb
Matthew Singer <73543914+MatthewJSinger@users.noreply.github.com>
2026-07-21 21:42:55 +0100

    Initial project setup

---
+++ src/main/java/org/moosedev/builderModeMC/BuilderModeMC.java
@@ -0,0 +1,16 @@
+package org.moosedev.builderModeMC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuilderModeMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
